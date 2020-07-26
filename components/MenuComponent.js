a292f43ce867039704759c9c7f904fbacfd29927
Chirag Jagad <chiragjagad08@gmail.com>
2020-07-27 03:42:51 +0530

    Save 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,48 +1,65 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { DISHES } from '../shared/dishes';
+import { FlatList } from 'react-native';
+import { Tile } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes
+    }
+}
 
 class Menu extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES
-        };
-    }
+
 
     static navigationOptions = {
         title: 'Menu'
     };
 
+
+
     render() {
+        const renderMenuItem = ({ item, index }) => {
 
-    const { navigate } = this.props.navigation;
-
-    const renderMenuItem = ({item, index}) => {
-
-        return (
-            <ListItem
-            key={index}
-            title={item.name}
-            subtitle={item.description}
-            hideChevron={true}
-            onPress={() => navigate('Dishdetail', { dishId: item.id })}
-            leftAvatar={{ source: require('./images/uthappizza.png')}}
-          />
-        );
-    };
-
-    return (
-            <FlatList 
-            data={this.state.dishes}
-            renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
+            return (
+                <Tile
+                    key={index}
+                    title={item.name}
+                    caption={item.description}
+                    featured
+                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                    imageSrc={{ uri: baseUrl + item.image }}
                 />
-    );
-}
+            );
+        };
+
+        const { navigate } = this.props.navigation;
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else {
+            return (
+                <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            );
+        }
+    }
 }
 
 
-export default Menu;
+export default connect(mapStateToProps)(Menu);
---
+++ shared/baseUrl.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'http://192.168.43.99:3001/'
+
+//json-server --watch db.json -p 3001 -H 192.168.43.99 -d 2000
